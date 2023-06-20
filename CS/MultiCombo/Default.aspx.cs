a855f2b4e5066d9c01dda5483bb3c1ff63ff5f19
elenapeskova <Elena.Peskova@devexpress.com>
2023-06-20 10:06:15 +0400

    update code

--- CS/MultiCombo/Default.aspx.cs
+++ CS/MultiCombo/Default.aspx.cs
@@ -1,42 +1,23 @@
 using System;
-using System.Data;
-using System.Configuration;
-using System.Collections;
-using System.Web;
-using System.Web.Security;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Web.UI.HtmlControls;
 using DevExpress.Web;
-using System.Collections.Generic;
 using System.Collections.Specialized;
 
-namespace MultiCombo
-{
-    public partial class _Default : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Cat1_SelectedIndexChanged(object sender, EventArgs e)
-        {
+namespace MultiCombo {
+    public partial class _Default : System.Web.UI.Page {
+        protected void Cat1_SelectedIndexChanged(object sender, EventArgs e) {
             ASPxComboBox combo1 = (ASPxComboBox)sender;
             object oldCat1 = Session["Cat1ID"];
-            if (oldCat1 != null && oldCat1.Equals(combo1.Value)) return;
+            if(oldCat1 != null && oldCat1.Equals(combo1.Value)) return;
             Session["Cat1ID"] = combo1.Value;
             ASPxComboBox combo2 = ((ASPxComboBox)grid.FindEditRowCellTemplateControl(
                 grid.Columns["Category2ID"] as GridViewDataComboBoxColumn, "Cat2"));
             combo2.Value = null;
             Cat2_SelectedIndexChanged(combo2, EventArgs.Empty);
         }
-        protected void Cat2_SelectedIndexChanged(object sender, EventArgs e)
-        {
+        protected void Cat2_SelectedIndexChanged(object sender, EventArgs e) {
             ASPxComboBox combo2 = (ASPxComboBox)sender;
             object oldCat2 = Session["Cat2ID"];
-            if (oldCat2 != null && oldCat2.Equals(combo2.Value)) return;
+            if(oldCat2 != null && oldCat2.Equals(combo2.Value)) return;
             Session["Cat2ID"] = combo2.Value;
             ASPxComboBox combo3 = ((ASPxComboBox)grid.FindEditRowCellTemplateControl(
                 grid.Columns["Category3ID"] as GridViewDataComboBoxColumn, "Cat3"));
@@ -44,8 +25,7 @@ namespace MultiCombo
             Cat3_SelectedIndexChanged(combo3, EventArgs.Empty);
         }
 
-        protected void Cat3_SelectedIndexChanged(object sender, EventArgs e)
-        {
+        protected void Cat3_SelectedIndexChanged(object sender, EventArgs e) {
             ASPxComboBox combo3 = (ASPxComboBox)sender;
             object oldCat3 = Session["Cat3ID"];
             if(oldCat3 != null && oldCat3.Equals(combo3.Value)) return;
@@ -55,14 +35,13 @@ namespace MultiCombo
             combo4.Value = null;
         }
 
-        protected void grid_HtmlRowCreated(object sender, DevExpress.Web.ASPxGridViewTableRowEventArgs e)
-        {
-            if ((e.RowType == GridViewRowType.InlineEdit)) {
+        protected void grid_HtmlRowCreated(object sender, DevExpress.Web.ASPxGridViewTableRowEventArgs e) {
+            if((e.RowType == GridViewRowType.InlineEdit)) {
                 ASPxComboBox combo2 = ((ASPxComboBox)grid.FindEditRowCellTemplateControl(
                     grid.Columns["Category2ID"] as GridViewDataComboBoxColumn, "Cat2"));
-                if (Request.Params[combo2.UniqueID] != null) return;
+                if(Request.Params[combo2.UniqueID] != null) return;
 
-                if (!grid.IsNewRowEditing) {
+                if(!grid.IsNewRowEditing) {
                     object[] vals = (object[])grid.GetRowValues(grid.EditingRowVisibleIndex,
                     new string[] { "Category2ID", "Category3ID", "Category4ID" });
                     ASPxComboBox combo4 = ((ASPxComboBox)grid.FindEditRowCellTemplateControl(
@@ -73,26 +52,22 @@ namespace MultiCombo
                     combo3.Value = vals[1];
                     combo4.Value = vals[2];
                 }
-
             }
         }
 
-        protected void grid_StartRowEditing(object sender, DevExpress.Web.Data.ASPxStartRowEditingEventArgs e)
-        {
-            object[] vals = (object[]) grid.GetRowValuesByKeyValue(e.EditingKeyValue, 
-                new string[] {"Category1ID", "Category2ID", "Category3ID" });
+        protected void grid_StartRowEditing(object sender, DevExpress.Web.Data.ASPxStartRowEditingEventArgs e) {
+            object[] vals = (object[])grid.GetRowValuesByKeyValue(e.EditingKeyValue,
+                new string[] { "Category1ID", "Category2ID", "Category3ID" });
             Session["Cat1ID"] = vals[0];
             Session["Cat2ID"] = vals[1];
             Session["Cat3ID"] = vals[2];
         }
 
-        protected void grid_RowUpdating(object sender, DevExpress.Web.Data.ASPxDataUpdatingEventArgs e)
-        {
+        protected void grid_RowUpdating(object sender, DevExpress.Web.Data.ASPxDataUpdatingEventArgs e) {
             SaveOrInsertData(e.NewValues);
         }
 
-        private void SaveOrInsertData(OrderedDictionary NewValues)
-        {
+        private void SaveOrInsertData(OrderedDictionary NewValues) {
             object cat1 = ((ASPxComboBox)grid.FindEditRowCellTemplateControl(
                 grid.Columns["Category1ID"] as GridViewDataComboBoxColumn, "Cat1")).Value;
             NewValues["Category1ID"] = cat1;
@@ -107,10 +82,8 @@ namespace MultiCombo
             NewValues["Category4ID"] = cat4;
         }
 
-        protected void grid_RowInserting(object sender, DevExpress.Web.Data.ASPxDataInsertingEventArgs e)
-        {
+        protected void grid_RowInserting(object sender, DevExpress.Web.Data.ASPxDataInsertingEventArgs e) {
             SaveOrInsertData(e.NewValues);
         }
-
     }
 }
